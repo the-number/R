d14cdcf2795a18208b1bc294db1e1249ac3fffa5
John Darrington <john@darrington.wattle.id.au>
2010-11-07 11:22:20 +0100

    Automatically generate the ChangeLog file

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,8 @@
-mkdir -p build-aux
 mkdir -p m4
 aclocal -I m4 
 autoconf
 autoheader
+build-aux/gitlog-to-changelog > ChangeLog
+cat ChangeLog.OLD >> ChangeLog
 automake --add-missing --copy
 ls -1 src/*.c > po/POTFILES.in
